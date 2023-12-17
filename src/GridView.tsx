190e705c8df78ff2982bbcab7026c156b0e5fcfe
h4rkon <victor.sauermann@gmail.com>
2023-12-17 08:16:00 +0100

    Introduced collapsibles

--- src/GridView.tsx
+++ src/GridView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './GridView.css'; // Assuming you have a CSS file for styles
+import Collapsible from 'react-collapsible';
 
 
 const GridView = () => {
@@ -7,51 +8,61 @@ const GridView = () => {
     {
       group: {
         name: 'Group 1', content: [
-          { key: { name: 'Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1', 'Value1.2', 'Value1.3'] },
-          { key: { name: 'Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.1', 'Value2.2', 'Value2.3'] },
-          { key: { name: 'Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value3.1', 'Value3.2', 'Value3.3'] }
+          { key: { name: 'Group1 - Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1.1', 'Value1.1.2', 'Value1.1.3'] },
+          { key: { name: 'Group1 - Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.2.1', 'Value1.2.2', 'Value1.2.3'] },
+          { key: { name: 'Group1 - Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.3.1', 'Value1.3.2', 'Value1.3.3'] },
+          { key: { name: 'Group1 - Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1.1', 'Value1.1.2', 'Value1.1.3'] },
+          { key: { name: 'Group1 - Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.2.1', 'Value1.2.2', 'Value1.2.3'] },
+          { key: { name: 'Group1 - Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.3.1', 'Value1.3.2', 'Value1.3.3'] },
+          { key: { name: 'Group1 - Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1.1', 'Value1.1.2', 'Value1.1.3'] },
+          { key: { name: 'Group1 - Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.2.1', 'Value1.2.2', 'Value1.2.3'] },
+          { key: { name: 'Group1 - Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.3.1', 'Value1.3.2', 'Value1.3.3'] },
+          { key: { name: 'Group1 - Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1.1', 'Value1.1.2', 'Value1.1.3'] },
+          { key: { name: 'Group1 - Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.2.1', 'Value1.2.2', 'Value1.2.3'] },
+          { key: { name: 'Group1 - Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.3.1', 'Value1.3.2', 'Value1.3.3'] }
         ]
       },
     },
     {
       group: {
         name: 'Group 2', content: [
-          { key: { name: 'Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1', 'Value1.2', 'Value1.3'] },
-          { key: { name: 'Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.1', 'Value2.2', 'Value2.3'] },
-          { key: { name: 'Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value3.1', 'Value3.2', 'Value3.3'] }
+          { key: { name: 'Group2 - Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.1.1', 'Value2.1.2', 'Value2.1.3'] },
+          { key: { name: 'Group2 - Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.2.1', 'Value2.2.2', 'Value2.2.3'] },
+          { key: { name: 'Group2 - Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.3.1', 'Value2.3.2', 'Value2.3.3'] }
         ]
       },
     },
   ];
 
-  const data = [
-    { key: { name: 'Key1', description: 'Lorem ipsum dolor sit amet' }, value: ['Value1.1', 'Value1.2', 'Value1.3'] },
-    { key: { name: 'Key2', description: 'Lorem ipsum dolor sit amet' }, value: ['Value2.1', 'Value2.2', 'Value2.3'] },
-    { key: { name: 'Key3', description: 'Lorem ipsum dolor sit amet' }, value: ['Value3.1', 'Value3.2', 'Value3.3'] }
-
-  ];
-
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Header</th>
-          <th>At Dispatch</th>
-          <th>At Submission</th>
-          <th>At Approval/Rejection</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.key.name}<br /><div className="description">{item.key.description}</div></td>
-            {item.value.map((val, valIndex) => (
-              <td key={valIndex}>{val}</td>
-            ))}
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Header</th>
+            <th>At Dispatch</th>
+            <th>At Submission</th>
+            <th>At Approval/Rejection</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+      </table>
+      {data_new.map((group, index) => (
+        <Collapsible key={index} trigger={group.group.name}>
+          <table>
+            <tbody>
+              {group.group.content.map((item, itemIndex) => (
+                <tr key={index}>
+                  <td>{item.key.name}<br /><div className="description">{item.key.description}</div></td>
+                  {item.value.map((val, valIndex) => (
+                    <td key={valIndex}>{val}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Collapsible>
+      ))}
+    </div>
   );
 };
 
